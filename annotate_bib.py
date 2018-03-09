6086d8ff85cf1b28e32cb541347e6445c967143a
Jarin French <jarinf@vt.edu>
2018-03-08 19:26:31 -0500

    Added capability to cycle through entries in a .bib file.  Working on the saving function.  Added some platform-specific commands.

--- annotate_bib.py
+++ annotate_bib.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 from __future__ import print_function
 import sys
@@ -10,59 +10,63 @@ else:
     from tkinter import messagebox
     from tkinter import filedialog
 import os
+import platform
 import re
-import collections
+import copy
 
 
 class App(object):
     def __init__(self, master):
-        self.fields = ['Author(s)', 'Title', 'Journal', 'Year', 
+        self.fields = ['Author(s)', 'Title', 'Journal', 'Year',
                        'Volume', 'Issue', 'Pages']
-        self.data_keys = ["AUTHOR", "TITLE", "JOURNAL", "YEAR", "VOLUME", 
+        self.data_keys = ["AUTHOR", "TITLE", "JOURNAL", "YEAR", "VOLUME",
                           "ISSUE", "PAGES", "SUMMARY", "CRITIQUE", "RELEVANCE"]
-        self.data = collections.OrderedDict({"AUTHOR": StringVar(), "TITLE": StringVar(), 
+        self.data = {"AUTHOR": StringVar(), "TITLE": StringVar(),
                      "JOURNAL": StringVar(), "YEAR": StringVar(),
-                     "VOLUME": StringVar(), "ISSUE": StringVar(), 
+                     "VOLUME": StringVar(), "ISSUE": StringVar(),
                      "PAGES": StringVar(), "SUMMARY": StringVar(),
-                     "CRITIQUE": StringVar(), "RELEVANCE": StringVar()})
+                     "CRITIQUE": StringVar(), "RELEVANCE": StringVar()}
         self.textFields = ['Summary', 'Critique', 'Relevance']
-        self.myFileTypes = [("Annotated bibliography files", "*.anbib"), 
-                            ("Text documents", "*.txt"), 
-                            ("Bibliography files", "*.bib"), 
+        self.myFileTypes = [("Annotated bibliography files", "*.anbib"),
+                            ("Text documents", "*.txt"),
+                            ("Bibliography files", "*.bib"),
                             ("All file types", "*.*")]
         self.windowOpen = False
         self.entries = {}
         self.textEntries = {}
-        
+
         # Set keybindings
         master.bind("<Control-n>", self.openNew)
         master.bind("<Control-s>", self.saveFile)
+        master.bind("<Control-Shift-s>", self.saveFileAs)
         master.bind("<Control-o>", self.openFile)
         master.bind("<Control-w>", self.on_exit)
+        master.bind("<Control-e>", self.exportBib)
         root.protocol("WM_DELETE_WINDOW", self.on_exit) # exit protocol
-        #self.frame = Frame(master)
-        #self.frame.place(relwidth = 0.5, relheight = 0.5, )
 
         self.menu = Menu(root)
         root.config(menu=self.menu)
         self.menu.filemenu = Menu(self.menu)
         self.menu.add_cascade(label="File", menu=self.menu.filemenu)
-        self.menu.filemenu.add_command(label="New", command = self.openNew)
-        self.menu.filemenu.add_command(label="Open", command = self.openFile)
-        self.menu.filemenu.add_command(label = "Save", command = self.saveFile, 
+        self.menu.filemenu.add_command(label= "New", command = self.openNew, accelerator = "Ctrl+N")
+        self.menu.filemenu.add_command(label= "Open", command = self.openFile, accelerator = "Ctrl+O")
+        self.menu.filemenu.add_command(label = "Save", command = self.saveFile, accelerator = "Ctrl+S",
                                        state = "disabled")
-        self.menu.filemenu.add_command(label = "Save As", 
-                                       command = self.saveFile, 
+        self.menu.filemenu.add_command(label = "Save As",
+                                       command = self.saveFile, accelerator = "Ctrl+Shift+S",
                                        state = "disabled")
-        self.menu.filemenu.add_command(label="Export Bibliography", 
-                                       command = self.exportBib)
+        self.menu.filemenu.add_command(label= "Export Bibliography",
+                                       command = self.exportBib, accelerator = "Ctrl+E")
         self.menu.filemenu.add_separator()
-        self.menu.filemenu.add_command(label="Quit", command = root.destroy)
+        self.menu.filemenu.add_command(label= "Quit", command = root.destroy)
+
+        self.menu.bibtexMenu = Menu(self.menu)
+        self.menu.add_cascade(label = "BibTex Entries", menu = self.menu.bibtexMenu, state = "disabled")
 
         self.menu.helpmenu = Menu(self.menu)
         self.menu.add_cascade(label = "Help", menu = self.menu.helpmenu)
         self.menu.helpmenu.add_command(label = "About...", command = self.askAbout)
-    
+
     def on_exit(self, event = None):
         if messagebox.askyesno("Exit", "Do you want to quit the application?"):
             root.destroy()
@@ -101,14 +105,11 @@ class App(object):
     def openFile(self, event = None):
         self.filename = filedialog.askopenfilename(initialdir = "~",
                         title = "Select file", filetypes = self.myFileTypes)
-        if not self.windowOpen:
-            self.openNew()
-            self.windowOpen = True
-        if self.filename is not '':
-            for key in self.textEntries.keys():
-                self.textEntries[key].delete(1.0, END)
-            for key in self.entries.keys():
-                self.entries[key].delete(0, END)
+        print(type(self.filename))
+        if not self.filename in ['', None]:
+            if not self.windowOpen:
+                self.openNew()
+                self.windowOpen = True
             text = self.readInFile()
             self.parseFile(text)
 
@@ -122,10 +123,15 @@ class App(object):
         return text
 
     def parseFile(self, text):
-        field_strings = ["AUTHOR", "TITLE", "JOURNAL", "YEAR", 
-                         "VOLUME", "ISSUE", "PAGES", "SUMMARY", 
+        field_strings = ["AUTHOR", "TITLE", "JOURNAL", "YEAR",
+                         "VOLUME", "ISSUE", "PAGES", "SUMMARY",
                          "CRITIQUE", "RELEVANCE"]
+
         def populateFields(dataset):
+            for key in self.textEntries.keys():
+                self.textEntries[key].delete(1.0, END)
+            for key in self.entries.keys():
+                self.entries[key].delete(0, END)
             self.entries["Author(s)"].insert(END, dataset["AUTHOR"].get())
             self.entries["Title"].insert(END, dataset["TITLE"].get())
             self.entries["Journal"].insert(END, dataset["JOURNAL"].get())
@@ -136,7 +142,7 @@ class App(object):
             self.textEntries["Summary"].insert(END, dataset["SUMMARY"].get())
             self.textEntries["Critique"].insert(END, dataset["CRITIQUE"].get())
             self.textEntries["Relevance"].insert(END, dataset["RELEVANCE"].get())
-            
+
         def parseAnbib(text):
             data = []
             j = 0
@@ -153,7 +159,7 @@ class App(object):
                 if sub_data[0] in field_strings:
                     self.data[sub_data[0]].set(sub_data[1].replace("\n", ' '))
             populateFields(self.data)
-        
+
         def parseTxt(text):
             data = []
             j = 0
@@ -170,18 +176,22 @@ class App(object):
             for data_key, data_val in data:
                 self.data[data_key].set(data_val)
             populateFields(self.data)
-        
+
         def parseBib(text):
             # Note that a bib file may contain multiple entries!  In this case,
             # should I open up a separate file for each of them?  Or should I
             # prompt the user for which data entry to edit?
-            # For now, here is a one-entry bib file.            
+            # For now, here is a one-entry bib file.
             text = re.sub("\n",' ',' '.join(text))
             bib_labels = re.findall(r"@(.*?){\s*?(.*?),", text)
-            data = [self.data] * len(bib_labels)
-            self.menu.bibtexMenu = Menu(self.menu)
-            self.menu.add_cascade(label = "BibTex Entries", menu = self.menu.bibtexMenu)
-            
+            data = []
+            for i in range(len(bib_labels)):
+                data.append({"AUTHOR": StringVar(), "TITLE": StringVar(),
+                             "JOURNAL": StringVar(), "YEAR": StringVar(),
+                             "VOLUME": StringVar(), "ISSUE": StringVar(),
+                             "PAGES": StringVar(), "SUMMARY": StringVar(),
+                             "CRITIQUE": StringVar(), "RELEVANCE": StringVar()})
+
             read_error = '''Error reading file %s.
 Make sure the first line of the file is specified in .bib format! (@type{...'''%self.filename
             regexs = [r"(?i)author\s*?=\s*?([{\"].*?[}\"])",
@@ -196,49 +206,77 @@ Make sure the first line of the file is specified in .bib format! (@type{...'''%
                       r"(?i)relevance(:|(\s*?-))\s*?(.*?(?=(critique|summary|@|$)))"]
             if not text.startswith("@"):
                 messagebox.showerror("Read Error", read_error)
-                # read each line until we either a) come to two } symbols next
-                # to each other, b) come to a "} set of symbols, or c) come to
-                # a line that starts with a }
             for i in range(len(regexs)):
                 result = re.findall(regexs[i], text)
-                print(result)
                 if result:
                     for j in range(len(result)):
                         if i < 7:
                             # Make sure the brackets or quotation marks match!
-                            if (result[j].group(1).startswith("{") and 
-                            result[j].group(1).endswith("}")) or \
-                            (result[j].group(1).startswith("\"") and 
-                            result[j].group(1).endswith("\"")):
-                                data[j][self.data_keys[i]].set(result[j].group(1)[1:-1])
+                            if (result[j].startswith("{") and
+                            result[j].endswith("}")) or \
+                            (result[j].startswith("\"") and
+                            result[j].endswith("\"")):
+                                data[j][self.data_keys[i]].set(result[j][1:-1])
                         else: # summary, critique, and relevance
-                            data[j][self.data_keys[i]].set((result[j].group(3)).strip())
-                    #print (self.data[self.data_keys[i]].get())
+                            data[j][self.data_keys[i]].set(result[j][2].strip())
             for i in range(len(bib_labels)):
                 self.menu.bibtexMenu.add_command(label = bib_labels[i][1],
-                                                 command = (lambda: populateFields(data[i])))
-                
+                                                 command = (lambda i=i: populateFields(data[i])))
 
+            populateFields(data[0])
 
         file_ext = os.path.splitext(self.filename)[1]
         if (file_ext == ".anbib"):
+            self.menu.entryconfig("BibTex Entries", state = "disabled")
             parseAnbib(text)
         elif(file_ext == ".txt"):
+            self.menu.entryconfig("BibTex Entries", state = "disabled")
             parseTxt(text)
         elif(file_ext == ".bib"):
-            print("Reading a LaTeX Bibliography file.")
+            self.menu.entryconfig("BibTex Entries", state = "normal")
             parseBib(text)
         else:
             print("Reading an unknown file format")
             #parseOther(text)
 
     def saveFile(self, event = None):
-        save_dialog = filedialog.asksaveasfilename(root, initialdir = "~",
-                        title = "Select save destination", filetypes = self.myFileTypes)
-        statusbar = label(root, text = "", bd = 1, relief = SUNKEN, anchor = W)
-        statusbar.pack(side = BOTTOM, fill = X)
-        statusbar.config(text = save_dialog.show())
-        print("Saving file")
+        try:
+            ext = os.path.splitext(self.filename)[1]
+            with open(self.filename, 'w') as fout:
+                if ext == ".anbib":
+                    self.saveAsAnBib(fout)
+                elif ext == ".txt":
+                    self.saveAsTxt(fout)
+                elif ext == ".bib":
+                    self.saveAsBib(fout)
+                else:
+                    messagebox.showerror("Could Not Save File!", "Could not save file %s"%self.filename)
+        except AttributeError:
+            self.saveFileAs()
+
+    def saveFileAs(self, event = None):
+        self.filename = filedialog.asksaveasfilename(initialdir = "~",
+                        title = "Select save destination", filetypes = self.myFileTypes,
+                        defaultextension = ".anbib")
+        ext = os.path.splitext(self.filename)[1]
+        with open(self.filename, 'w') as fout:
+            if ext == ".anbib":
+                self.saveAsAnBib(fout)
+            elif ext == ".txt":
+                self.saveAsTxt(fout)
+            elif ext == ".bib":
+                self.saveAsBib(fout)
+            else:
+                messagebox.showerror("Could Not Save File!", "Could not save file %s"%self.filename)
+
+    def saveAsAnBib(self, fout):
+        print("Saving as %s"%self.filename)
+
+    def saveAsTxt(self, fout):
+        print("Saving as %s"%self.filename)
+
+    def saveAsBib(self, fout):
+        print("Saving as %s"%self.filename)
 
     def exportBib(self):
         msg = "Bibliography exported to annotated_bib.bib"
@@ -276,7 +314,10 @@ if __name__ == "__main__":
     #m = root.maxsize()
     #print(m)
     #root.geometry('{}x{}+0+0'.format(*m))
-    root.wm_state("zoomed")
+    if platform.system() == "Windows":
+        root.wm_state("zoomed")
+    elif platform.system() in ["Linux", "Darwin"]:
+        root.attributes('-zoomed', True)
     #screen_width = root.winfo_screenwidth()
     #screen_height = root.winfo_screenheight()
     #root.geometry('%sx%s'%(screen_width, screen_height))
